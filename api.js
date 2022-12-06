43b9d5985dcb6bf6d1aaf4de56587ec2fca8866b
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-12-06 18:41:38 +0900

    day3

---
+++ api.js
@@ -0,0 +1,97 @@
+const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "http://127.0.0.1:8000"
+
+window.onload = () => {
+    console.log("로딩되었음")
+}
+
+async function handleSignin() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    console.log(email, password)
+
+
+    const response = await fetch(`${backend_base_url}/users/signup/`, {
+        headers: {
+            'content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "password": password
+        })
+    })
+
+    return response
+
+
+}
+
+async function handleLogin() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    const response = await fetch('http://127.0.0.1:8000/users/api/token/', {
+        headers: {
+            'content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "password": password
+        })
+    })
+
+
+    if (response.status == 200) {
+
+        const response_json = await response.json()
+        console.log(response)
+        console.log(response_json)
+
+        localStorage.setItem("access", response_json.access);
+        localStorage.setItem("refresh", response_json.refresh);
+
+
+
+        const base64Url = response_json.access.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        localStorage.setItem("payload", jsonPayload);
+        alert("환영합니다!")
+        window.location.replace(`${frontend_base_url}/`)
+
+    } else {
+        alert("회원정보가 일치하지 않습니다!")
+    }
+
+
+
+
+}
+
+
+
+
+async function handleMock() {
+    const response = await fetch('http://127.0.0.1:8000/users/mock/', {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("access")
+        },
+        method: 'GET',
+    })
+
+    console.log(response)
+}
+
+
+function handleLogout() {
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    localStorage.removeItem("payload")
+    location.reload()
+
+}
---
+++ signup.js
@@ -0,0 +1,9 @@
+async function handleSigninButton() {
+    const response = await handleSignin();
+
+    if (response.status == 201) {
+        alert("회원가입을 축하합니다!")
+        window.location.replace(`${frontend_base_url}/login.html`)
+    }
+
+}
